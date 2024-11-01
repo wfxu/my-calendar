9263d78e83f9a9ba4d071ab9f8a03db2c343c796
wfxu <wenfenxu@qq.com>
2024-11-01 11:17:55 +0800

    删除不必要组件

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,26 +1,8 @@
 import { tasks } from '@/lib/mock-data'
 import { Task } from '@/lib/definitions';
 
-function isSameDay(date1: Date, date2: Date): boolean {
-    return date1.getFullYear() === date2.getFullYear() &&
-           date1.getMonth() === date2.getMonth() &&
-           date1.getDate() === date2.getDate();
-  }
-  
-function isSameMonth(date1: Date, date2: Date): boolean {
-return date1.getFullYear() === date2.getFullYear() &&
-        date1.getMonth() === date2.getMonth();
-}
 
-function getTodayCompletedTasks(): Task[] {
-const today = new Date();
-return tasks.filter(task => task.isCompleted && isSameDay(task.completedAt!, today));
-}
 
-function getMonthCompletedTasks(): Task[] {
-const today = new Date();
-return tasks.filter(task => task.isCompleted && isSameMonth(task.completedAt!, today));
-}
 
 function getUncompletedTasks(): Task[] {
 const today = new Date();
@@ -32,4 +14,4 @@ const today = new Date();
 return tasks.filter(task => !task.isCompleted && task.createdAt > today);
 }
 
-export { getTodayCompletedTasks, getMonthCompletedTasks, getUncompletedTasks, getPendingTasks }
+export { getUncompletedTasks, getPendingTasks }
