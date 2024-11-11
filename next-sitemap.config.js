dab476facf8d63d2893ba54af9137ad8e1858c29
wfxu <wenfenxu@qq.com>
2024-11-11 16:33:02 +0800

    更新站点地图

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://calendar.wfxu.com',
+    siteUrl: process.env.SITE_URL || 'https://calendar.wfxu.one',
     generateRobotsTxt: true, // (optional)
     // ...other options
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ['/login'],
+    robotsTxtOptions: {
+        additionalSitemaps: [
+        'https://calendar.wfxu.one/sitemap.xml',
+        ],
+    },
   }
   
