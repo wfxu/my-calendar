ed1941220d0ec3017979f9138074c8bdfbde7967
wfxu <wenfenxu@qq.com>
2024-11-11 15:54:04 +0800

    添加google流量统计

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,49 @@
+'use client';
+
+import React, { useEffect } from 'react';
 import "@/ui/globals.css";
 
+declare global {
+  interface Window {
+    dataLayer: Array<Record<string, unknown>>;
+    gtag: (...args: GtagFunctionArgs) => void;
+  }
+}
+
+type GtagFunctionArgs =
+  | ['js', Date]
+  | ['config', string, Record<string, unknown>?];
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    // Load Google Analytics script
+    const script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-D8RYGRY0ZR";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Initialize Google Analytics
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: GtagFunctionArgs) {
+        window.dataLayer.push(args);
+      }
+      window.gtag = gtag;
+
+      window.gtag('js', new Date());
+      window.gtag('config', 'G-D8RYGRY0ZR');
+    };
+
+    // Cleanup function to remove the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <body>
         {children}
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-D8RYGRY0ZR"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-D8RYGRY0ZR');
-        </script>
       </body>
     </html>
   );
